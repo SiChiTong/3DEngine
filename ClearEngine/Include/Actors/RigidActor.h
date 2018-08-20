41d7fa5a3f013e529f6a5894f193eb515839de02
Aidan Clear <aidanclear@gmail.com>
2018-08-20 17:59:31 +0100

    Adding engine actor

--- ClearEngine/Include/Actors/RigidActor.h
+++ ClearEngine/Include/Actors/RigidActor.h
@@ -1,28 +1,18 @@
 #pragma once
 
 #include "CActor.h"
-#include "Graphics/Actors/RigidGraphicsActor.h"
-#include "Geometry/Models.h"
-#include "Physics/CPhysics.h"
 
 class RigidActor : public CActor
 {
 	public:
-		RigidActor(std::wstring name, DirectX::XMFLOAT3 scale, DirectX::XMFLOAT4 rotation, DirectX::XMFLOAT3 position, RigidModel* model, physx::PxRigidActor* physicsActor = nullptr);
-		~RigidActor();
-		void Update(double time);
-		RigidModel* GetModel();
-		ModelSubset* GetModelSubset(int subsetIndex);  
+		virtual ~RigidActor();
+		virtual void Update(double time);
 		CActorType GetType();
-		physx::PxRigidActor* GetPhysicsActor();
 		void SetScale(DirectX::XMFLOAT3 scale);
 		void SetRotation(DirectX::XMFLOAT4 rotation);
 		void SetPosition(DirectX::XMFLOAT3 position);
 
-	private:
-		RigidModel* model;
-		physx::PxRigidActor* physicsActor;
-		RigidGraphicsActor* graphicsActor;
-
+	protected:
+		RigidActor(std::wstring name, DirectX::XMFLOAT3 scale, DirectX::XMFLOAT4 rotation, DirectX::XMFLOAT3 position);
 };
 
